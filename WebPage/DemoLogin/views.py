b6feaf3b41bbd6de47a36f6849d7f30344d8c2e0
Carlos Jatib <cjatib@lc0161-1609.chile.taylor.cl>
2020-05-29 15:29:49 -0400

    Registro OK - Login retorna token, falta Redirect

--- WebPage/DemoLogin/views.py
+++ WebPage/DemoLogin/views.py
@@ -16,28 +16,33 @@ def index(request):
     return render(request, 'demologin/index.html', {})
 
 def login(request):
-    if request.method == 'GET':
-        nickname = request.GET.get('nickname')
-        password = request.GET.get('password')
-        email = request.GET.get('email')
-        name = request.GET.get('name')
-        family_name = request.GET.get('family_name')
-        #response = requests.post(url, json={'username': nickname, 'password': password, 'name': name, 'family_name': family_name}, headers=headers,allow_redirects=True)
-        #print(nickname)
-        #print(password)
-        print('Estado del codigo : ')
-        print(response.status_code)
+    if request.method == 'POST':
+        nickname = request.POST.get('nickname')
+        password = request.POST.get('password')
+        #email = request.GET.get('email')
+        #name = request.GET.get('name')
+        #family_name = request.GET.get('family_name')
+        url1="https://fnrryosh20.execute-api.us-east-1.amazonaws.com/Prod/login"
+        response = requests.post(url1, json={'username': nickname, 'password': password}, headers=headers)
+        print(nickname)
+        print(password)
+        #print('IMPRIMIR RESPONSE')
         #print(response.json())
-        json_data = response.json()
-        token = json_data.get('id_token')
+        print('Estado del codigo : ')
+        #print(response.status_code)
+        #print(response.json())
+        json_data = json.dumps(response.json())
+        data = json.loads(json_data)
+        #print(data)
+        token = data['data']['id_token']
         print('Imprimir Token: 1')    
         print(token)
         print(response.ok)
-        if response.ok and token != None:
+        #if response.ok and token != None:
             #response = response(headers={'Authorization': token},is_redirect=True,url="https://0q3wzpyww4.execute-api.us-east-1.amazonaws.com/prod/ping")
-            url1 = 'https://0q3wzpyww4.execute-api.us-east-1.amazonaws.com/prod/ping'
+            #url1 = 'https://0q3wzpyww4.execute-api.us-east-1.amazonaws.com/prod/ping'
             #header = {'Authorization': '{}'.format(token)}
-            headers1 = {"Authorization": token}
+            #headers1 = {"Authorization": token}
             #print('Imprimir Header 2')
             #print(header)
             #response = redirect(url1)
@@ -63,10 +68,10 @@ def login(request):
             #print(response)
             #request.redirect_to(url1)
             #response = requests.post(url1, headers=headers1)
-            context = { 'url' : 'https://0q3wzpyww4.execute-api.us-east-1.amazonaws.com/prod/ping',
-                'Authorization' : token
-            }
-            return HttpResponse(content=context)
+            #context = { 'url' : 'https://0q3wzpyww4.execute-api.us-east-1.amazonaws.com/prod/ping',
+            #    'Authorization' : token
+            #}
+            #return HttpResponse(content=context)
         #else:
         #    return render(request, 'demologin/login.html', {})
     
